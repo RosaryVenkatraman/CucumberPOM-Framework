f6acd708e3c3808f8aaea36b39e13fd3f7a85fbf
Rosary Venkatraman <rosary.venkatraman@Impigertech.com>
2020-03-19 23:16:22 +0530

    Modified Basepage

--- src/main/java/com/qa/pages/BasePage.java
+++ src/main/java/com/qa/pages/BasePage.java
@@ -1,17 +1,14 @@
 package com.qa.pages;
 
-import java.io.File;
+
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+
 import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
